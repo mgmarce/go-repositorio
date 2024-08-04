2d27defb04f459be43d16257e0b71b7a40c987b1
mgmarce <https.mgmarce@gmail.com>
2024-08-04 02:14:54 -0600

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mgmarce/go-repositorio
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mgmarce/go-repositorio/variables"
+)
+
+func main(){
+	variables.MostrarEnteros()
+}
